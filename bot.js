88d4c21a6a7814cfc04789199d8c4ad5182b0720
muhendisbirbey <42184009+muhendisbirbey@users.noreply.github.com>
2019-04-14 00:42:08 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,10 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-// const moment = require('moment');
-const prefix = '!';
+
 
 bot.on('ready', () => 
 	 {bot.user.setGame('Çalışmalar devam ediyor. Twitch kanalımıza gitmek için İZLE butonuna basabilirsin :) ', 'https://www.twitch.tv/muhendisbeymuhendishanim')});
    
-bot.on('message', msg => {	
-  if (msg.content === 's.a.' |msg.content === 's.a' |msg.content === 's' |msg.content === 'selm' |msg.content === 'salam' |msg.content === 'slm' |msg.content === 'sa' | msg.content ==='Sa'|msg.content ==='selamlar'|msg.content ==='sea'|msg.content ==='Sea' | msg.content ==='selam' | msg.content ==='Selamlar' | msg.content ==='Selam'){
-    msg.reply('Aleyküm Selam Hoş Geldin');
-  }
-});
+
 
 bot.login(process.env.BOT_TOKEN);
